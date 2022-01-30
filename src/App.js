726719297a71e9b8170ce7549110d1b06f66a799
Veronica Adler <vlynnadler@gmail.com>
2022-01-29 19:40:49 -0500

    added userefexample component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import StateHook from './components/UseStateHookExample'
 import ClassComponentWithState from './components/ClassComponentWithStateExample';
 import UseEffectExample from './components/UseEffectExample';
+import UseRefExample from './components/UseRefExample';
 
 function App() {
   return (
     <div className="App">
-      < StateHook />
+      <StateHook />
       <ClassComponentWithState />
       <UseEffectExample />
+      <UseRefExample />
     </div>
   );
 }
---
+++ src/components/UseRefExample.js
@@ -0,0 +1,31 @@
+import { useRef, useState } from "react";
+
+//Please note that useRef has several ways it can be used.  While not demonstrated here, it can also be used
+//to store references to DOM elements and allow you to, for example, utilize .focus() on that element.
+
+const UseRefExample = () => {
+    const prevCount = useRef(0); //returns a mutable object with a current property holding the current value.
+    //updating the value of useRef will not cause the component to re-render.  This is the main difference
+    //between a value held in useRef and one stored in useState. 
+    console.log(prevCount) 
+
+    const [ counter, setCounter ] = useState(0);
+    
+    return (
+        <div>
+            <h1>This is an example of the useRef hook.</h1>
+            <p>Count: {counter} Previous Count: {prevCount.current}</p>
+
+            <button 
+                onClick={() => setCounter(prevState => {
+                    prevCount.current = prevState;
+                    return prevState + 1
+                })}
+            >Increment
+            </button>
+            <button onClick={() => setCounter(prevState => prevState - 1)}>Decrement</button>
+        </div>
+      );
+}
+ 
+export default UseRefExample;
